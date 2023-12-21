78e9382fc2f2dd4609c6894654446d84ea7464e6
Thanos Kyriakakis <thanos.kyriakakis@persegroep.net>
2023-12-21 10:24:28 +0100

    connect repo to service

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
+      // strips out any additional unexpeted properties
       whitelist: true,
     }),
   );
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password);
     console.log('createUser body: ', body);
   }
 }
