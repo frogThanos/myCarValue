7f529f89d364f15a39d453f1e685af50551b32d8
Thanos Kyriakakis <thanos.kyriakakis@persegroep.net>
2023-12-21 11:52:00 +0100

    update UsersController to use UsersService

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Patch,
+  Param,
+  Query,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -9,5 +19,30 @@ export class UsersController {
   createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.email, body.password);
     console.log('createUser body: ', body);
+  }
+
+  // Nestjs does not automatically pars strings into numbers
+  // we need to do that ourselves
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    const parsId = parseInt(id);
+    return this.usersService.findOne(parsId);
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    const parsId = parseInt(id);
+    return this.usersService.update(parsId, body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    const parsId = parseInt(id);
+    return this.usersService.remove(parsId);
   }
 }
