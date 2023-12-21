dcec6c7a64ecebadf2fae20f4d2022f07cd438a3
Thanos Kyriakakis <thanos.kyriakakis@persegroep.net>
2023-12-21 10:43:01 +0100

    add tutorial comment :P

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,6 +8,10 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
+    // We need to create an instance of the User Entity
+    // before we save it using the repo. If we save it as
+    // a plain object typeorm hooks defined in the User Entity
+    // won't be executed.
     const user = this.repo.create({ email, password });
 
     return this.repo.save(user);
