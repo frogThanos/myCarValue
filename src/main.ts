3adb4f3d36d6920b51b2a4f258d94e34373e4b19
Thanos Kyriakakis <thanos.kyriakakis@persegroep.net>
2024-02-01 19:38:17 +0100

    install cookieSession

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import cookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['sdfsd'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       // strips out any additional unexpeted properties
