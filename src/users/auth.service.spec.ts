ad27fe2613654e64fb929172078e866293badf67
Thanos Kyriakakis <thanos.kyriakakis@persgroep.net>
2024-03-03 11:35:47 +0100

    update script and add comments

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,14 +1,15 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 it('can create an instance of the auth service', async () => {
   // create a mock of the UsersService
-  const mockUsersService = {
+  const mockUsersService: Partial<UsersService> = {
     // we only defined the functions that are used in the AuthService
     find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
+      Promise.resolve({ id: 1, email, password } as User),
   };
   const module = await Test.createTestingModule({
     providers: [
